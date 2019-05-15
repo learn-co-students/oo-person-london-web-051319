9d471acfc0cc81861adfcf88ef76e90ff80401e5
mbarrins <maija.barrins@gmail.com>
2019-05-15 15:45:48 +0100

    created instance methods

--- lib/person.rb
+++ lib/person.rb
@@ -31,5 +31,48 @@ class Person
     end
   end
 
+  def happy?
+    @happiness > 7
+  end
+  
+  def clean?
+    @hygiene > 7
+  end
+
+  def get_paid(amount)
+    @bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic == "politics"
+      self.happiness -= 2
+      person.happiness -= 2
+      return "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness += 1
+      person.happiness += 1
+      return "blah blah sun blah rain"
+    end
+    "blah blah blah blah blah"
+  end
+
 end
 # binding.pry
