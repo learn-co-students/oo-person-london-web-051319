f3de8c6eb882967400a0bdbc6a42ad821994ab87
mbarrins <maija.barrins@gmail.com>
2019-05-15 15:27:42 +0100

    added happiness & hygiene min/max condition on setter

--- lib/person.rb
+++ lib/person.rb
@@ -1,33 +1,35 @@
-# your code goes here
+require "pry"
+
 class Person
   attr_reader :name
-
   attr_accessor(:bank_account, :happiness, :hygiene)
 
   def initialize(name)
     @name = name
-    self.bank_account = 25
-    self.hygiene = 8
-    self.happiness = 8
+    @bank_account = 25
+    @hygiene = 8
+    @happiness = 8
   end
-  #
 
+  def happiness=(score)
+    if score > 10
+      @happiness = 10
+    elsif score < 0
+      @happiness = 0
+    else
+      @happiness = score
+    end
+  end
+  
+  def hygiene=(score)
+    if score > 10
+      @hygiene = 10
+    elsif score < 0
+      @hygiene = 0
+    else
+      @hygiene = score
+    end
+  end
 
-  # def happiness=(score)
-  #   if score (1..10)
-  #     self.happiness = score
-  # else
-  #   self.happiness = score > 10 ? 10 : 0
-  # end
-  # end
-  #
-  #   def hygiene= (score)
-  #     if score > 10
-  #       self.hygiene = 10
-  #     elsif score < 0
-  #       self.hygiene = 0
-  #     else
-  #       self.hygiene = score
-  #     end
-  #     end
 end
+# binding.pry
